d206ff313f4cfef0fdbbda39adafd3b5503b537b
Federico Visi <>
2023-11-29 12:47:47 +0100

    change default port and IP

--- wearable_computing_board/Network_Settings.h
+++ wearable_computing_board/Network_Settings.h
@@ -1,9 +1,9 @@
 #pragma once
 
 // OSC Ports
-#define OUTGOING_PORT 5001 //the UDP port the board is going to sream OSC data to
+#define OUTGOING_PORT 5000 //the UDP port the board is going to sream OSC data to
 #define ACTUATOR_PORT 2390 //used for the haptic motor
 
 // Computer IP
-#define COMPUTER_IP "192.168.1.13" //the local IP of the computer you want the board to send data to
+#define COMPUTER_IP "192.168.1.33" //the local IP of the computer you want the board to send data to
 
